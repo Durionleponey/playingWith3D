2a509627975093f578427a8402e205b38e7f8f83
robin <robin.clarembaux@gmail.com>
2026-09-02 17:48:15 +0200

    cube 🎉

--- index.js
+++ index.js
@@ -55,17 +55,24 @@ let angle = 0
 
 const vs = [
     {x:0.5,y:0.5,z:0.5},
+    {x:-0.5,y:0.5,z:0.5},
     {x:-0.5,y:-0.5,z:0.5},
     {x:0.5,y:-0.5,z:0.5},
-    {x:-0.5,y:0.5,z:0.5},
 
     {x:0.5,y:0.5,z:-0.5},
+    {x:-0.5,y:0.5,z:-0.5},
     {x:-0.5,y:-0.5,z:-0.5},
     {x:0.5,y:-0.5,z:-0.5},
-    {x:-0.5,y:0.5,z:-0.5},
 
+]
 
-
+const fs = [
+    [0,1,2,3],
+    [4,5,6,7],
+    [0,4],
+    [1,5],
+    [2,6],
+    [3,7]
 ]
 
 function translate_z({x,y,z},ofsetz){
@@ -84,20 +91,49 @@ function rotate_xz({x, y, z}, angle) {
 
 }
 
+function line(p1,p2){
+    ctx.lineWidth = 5
+    ctx.strokeStyle = FOREGROUND
+    ctx.beginPath()
+    ctx.moveTo(p1.x,p1.y)
+    ctx.lineTo(p2.x,p2.y)
+    ctx.stroke();
+
+
+
+}
+
 
 function frame(){
     //console.log(ofsetz)
-    ofsetz+=0.01
+    //ofsetz+=0.01
+
+    ofsetz = 2
 
     if (ofsetz>10){
         ofsetz=0.01
 
     }
     clear()
-    angle += 1
+    angle += 0.01
 
     for (let i of vs){
         point(screen(project((translate_z(rotate_xz(i,angle),ofsetz)))))
+    }
+
+    for (let f of fs){
+
+        for (let a =0;a<f.length;a++){
+
+            let aa = vs[f[a]]
+           let bb = vs[f[(a+1)%f.length]]
+
+            line(screen(project((translate_z(rotate_xz(aa,angle),ofsetz)))),
+                screen(project((translate_z(rotate_xz(bb,angle),ofsetz))))
+            )
+
+
+        }
     }
 
     setTimeout(frame,1000/FPS)
