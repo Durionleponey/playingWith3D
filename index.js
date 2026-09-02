903ccf61631466b6c417ead289369eeb83f74501
robin <robin.clarembaux@gmail.com>
2026-09-02 15:25:42 +0200

    converting -1..1 input to 0..W

--- index.js
+++ index.js
@@ -12,15 +12,27 @@ function clear(){
     ctx.fillRect(0,0,game.width,game.height)
 }
 
-function point(x,y) {
+function point({x,y}) {
     ctx.fillStyle = FOREGROUND
     ctx.fillRect(x,y,5,5)
 
 }
 
+
+function screen(p){
+
+    // -1..1 --> 0..2-> 0..1 -> 0..W
+
+    return {
+    x:(p.x + 1)/2*game.width,
+    y:(p.y + 1)/2*game.height
+    }
+
+}
+
+
 clear()
 
-point(10,10)
+point(screen({x:0,y:0}))
 
-point(15,10)
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+i just fallow this for fun
+
+https://www.youtube.com/watch?v=qjWkNZ0SXfo&vl=fr
